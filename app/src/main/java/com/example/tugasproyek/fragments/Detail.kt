7ed356b0912b545221714e5b5852f098156e64ff
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2022-07-02 18:01:28 +0700

    Mamo

--- app/src/main/java/com/example/tugasproyek/fragments/Detail.kt
+++ app/src/main/java/com/example/tugasproyek/fragments/Detail.kt
@@ -1,11 +1,14 @@
 package com.example.tugasproyek.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tugasproyek.NamaPemain
 import com.example.tugasproyek.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,6 +58,14 @@ class Detail : Fragment() {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createTeam.setOnClickListener {
+             startActivity(Intent(activity, NamaPemain::class.java))
             }
     }
 }
--- app/src/main/java/com/example/tugasproyek/fragments/ProfilFragment.kt
+++ app/src/main/java/com/example/tugasproyek/fragments/ProfilFragment.kt
@@ -10,10 +10,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.tugasproyek.*
 import com.example.tugasproyek.databinding.FragmentProfilBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profil.*
 import java.io.ByteArrayOutputStream
 
@@ -35,7 +38,9 @@ class ProfilFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
     private lateinit var imageUri : Uri
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,17 +90,74 @@ class ProfilFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val user = firebaseAuth.currentUser
+
+        if (user!=null){
+            if (user.photoUrl != null){
+                Picasso.get().load(user.photoUrl).into(fotoProfil)
+            }else{
+                Picasso.get().load("https://picsum.photos/id/237/200/300").into(fotoProfil)
+            }
+
+            etName.setText(user.displayName)
+            etEmail.setText(user.email)
+
+            if (user.isEmailVerified){
+                icVeri.visibility = View.VISIBLE
+
+            }else{
+                icUnveri.visibility = View.VISIBLE
+            }
+
+            if (user.phoneNumber.isNullOrEmpty()){
+                etPhone.setText("Masukkan Nomor Telepon")
+            }else{
+                etPhone.setText(user.phoneNumber )
+            }
+
+        }
+
         fotoProfil.setOnClickListener{
             intentCamera()
         }
 
-        //createTeam.setOnClickListener {
-           // startActivity(Intent(activity, NamaPemain::class.java))
-        //}
+        btnDetail.setOnClickListener {
+           startActivity(Intent(activity, NamaPemain::class.java))
+        }
 
         btnLogout.setOnClickListener {
             btnLogout()
         }
+        btnSave.setOnClickListener{
+            val image = when{
+                ::imageUri.isInitialized -> imageUri
+                user?.photoUrl == null -> Uri.parse("https://picsum.photos/id/237/200/300")
+                else -> user.photoUrl
+            }
+            val name = etName.text.toString().trim()
+
+            if(name.isEmpty()){
+                etName.error = "Nama Harus Diisi"
+                etName.requestFocus()
+                return@setOnClickListener
+            }
+            UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .setPhotoUri(image)
+                .build().also {
+                    user?.updateProfile(it)?.addOnCompleteListener {
+                        if (it.isSuccessful)(
+                                Toast.makeText(activity, "Profile Updated",Toast.LENGTH_SHORT).show()
+
+                        )else{
+                            Toast.makeText(activity, "${it.exception?.message}",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+        }
+
         
 //        firebaseAuth = FirebaseAuth.getInstance()
 //        btnLogout.setOnClickListener{
